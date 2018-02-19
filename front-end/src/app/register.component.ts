a7c2da1ba6513a5589620cffb7f115015ab1b1c4
ArsiaArdalan <parthiardalan@gmail.com>
2018-02-18 19:43:07 -0500

    validation in a diffrent way, through material api

--- front-end/src/app/register.component.ts
+++ front-end/src/app/register.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import * as _ from 'lodash';
+
 
 @Component({
     moduleId: module.id,
@@ -8,8 +10,23 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class RegisterComponent {
     email = new FormControl('', [Validators.required, Validators.email]);
+    firstName = new FormControl('', Validators.required);
+    lastName = new FormControl('', Validators.required);
+    password = new FormControl('', Validators.required);
+    confirmPassword = new FormControl('', Validators.required);
+
     getErrorMessage() {
         return this.email.hasError('required') ? 'You must enter a value' : this.email.hasError('email') ? 'Not a valid email' : '';
     }
+    getTextErrorMessage() {
+        return 'You must enter a value';
+    }
+    /**
+     * TODO: check passwords match
+     * disable register btn if one field is missing 
+     * 
+     */
+
+
 
 }
